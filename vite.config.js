da03d371df39a59d0baed93bab8a25b49129c088
Cristian Albarracin <denwa_cda@hotmail.com>
2024-07-22 17:00:42 -0300

    2daPreentrega6

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base:'/landing-albarracin/'
-=======
-  base:'/Landing-Albarracin/'
->>>>>>> c40e6614022e8b0e6cc4b0272430f89c721f03b7
 })
